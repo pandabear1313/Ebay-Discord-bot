3be8e1d8c91b4571652dbf3d24b1ffdaef79e269
pandabear1313 <shervinelibox569@gmail.com>
2026-01-02 20:05:15 -0400

    removed debug logic

--- debug_deps.js
+++
@@ -1,7 +0,0 @@
-try {
-    console.log('Requiring ebay-api...');
-    const eBayApi = require('ebay-api');
-    console.log('Success:', typeof eBayApi);
-} catch (e) {
-    console.error('FAIL:', e);
-}
--- debug_env.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-console.log("Dotenv Loaded");
-console.log("App ID:", process.env.EBAY_APP_ID);
-console.log("Secret:", process.env.EBAY_CLIENT_SECRET ? "HIDDEN" : "MISSING");
-console.log(
-  "RuName:",
-  process.env.EBAY_RU_NAME || process.env.EBAY_REDIRECT_URI || "MISSING"
-);
